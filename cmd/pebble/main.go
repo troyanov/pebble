0f47d0d486b3d8eda1a6b059bf85b7ad0ad9a8c9
Anton Troyanov <anton.troyanov@canonical.com>
2023-10-06 11:04:35 +0400

    wip: pprof

--- cmd/pebble/main.go
+++ cmd/pebble/main.go
@@ -16,12 +16,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"time"
+
+	"net/http"
+	_ "net/http/pprof"
+	"runtime/trace"
 
 	"github.com/canonical/pebble/internals/cli"
 )
 
 func main() {
+	traceFile := fmt.Sprintf("/var/snap/maas/common/pebble/%d.trace", time.Now().UnixNano())
+	f, err := os.Create(traceFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	trace.Start(f)
+	timer := time.NewTimer(5 * time.Minute)
+	go func() {
+		<-timer.C
+		trace.Stop()
+		log.Println("trace file", traceFile)
+	}()
+
+	runtime.SetMutexProfileFraction(5)
+	go func() {
+		fmt.Println(http.ListenAndServe(":6060", nil))
+	}()
+
 	if err := cli.Run(); err != nil {
 		fmt.Fprintf(cli.Stderr, "error: %v\n", err)
 		os.Exit(1)
